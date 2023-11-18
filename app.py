8a80a4ddff7f7ef78fc6c99f2eb48a5b3daee5b9
Josek <joseaveiro2001@gmail.com>
2023-11-18 12:54:03 -0300

    ruta edit habilitada

--- app.py
+++ app.py
@@ -2,36 +2,53 @@ from flask import Flask, render_template, request, redirect, url_for,flash
 from flask_login import LoginManager,login_user, logout_user, login_required, current_user
 from database import create_db
 from models import Task,User
-from forms import LoginForm, RegisterForm
+from forms import LoginForm, RegisterForm, TaskForm
 
 
+# Configuraciones 
 app = Flask(__name__)
-
 app.config.from_object("config.Config")
-
 db = create_db(app)
 
+
+
+
+# Configuracion del login 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
-
 @login_manager.user_loader
 def load_user(user_id):
     # Esta función carga el usuario a partir de su ID almacenado en la sesión
     return User.query.get(int(user_id))
 
 
+
+
+# Rutas 
 @app.route("/", methods=['GET','POST'])
 @login_required
 def index():
     tasks = Task.query.filter_by(user_id=current_user.id)
-    if request.method == "POST":
-        task = Task(title=request.form["titulo"], description=request.form["descripcion"],user_id=current_user.id)
+    form = TaskForm()
+    if form.validate_on_submit():
+        task = Task(title=form.title.data, description=form.description.data,user_id=current_user.id)
         db.session.add(task)
         db.session.commit()
-    
-    
-    return render_template("index.html", tasks= tasks)
+      
+    return render_template("index.html",tasks=tasks,form=form)
 
+
+@app.route("/edit/<int:id>", methods=["GET","POST"])
+def edit(id):
+    task = Task.query.get(id)
+    form = TaskForm(obj=task)
+    if form.validate_on_submit():
+        task.title = form.title.data
+        task.description = form.description.data
+        task.done = form.done.data
+        db.session.commit()
+        return redirect(url_for("index"))
+    return render_template("edit.html",form=form,task=task)
 
 
 
@@ -54,11 +71,19 @@ def login():
     return render_template('login.html', form=form)
 
 
+
+
+
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+
+
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -75,6 +100,9 @@ def register():
         
 
     return render_template("register.html",form=form)
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField,BooleanField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
@@ -12,3 +12,7 @@ class RegisterForm(FlaskForm):
     password = PasswordField('Contraseña', validators=[DataRequired()])
     submit = SubmitField('Registrarse')
 
+class TaskForm(FlaskForm):
+    title = StringField("Titulo", validators=[DataRequired()])
+    description = StringField("Descripcion",validators=[DataRequired()])
+    done = BooleanField("Completado")
